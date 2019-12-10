864b3ee9aaa6d988af1f06d47a9c03663d9752fb
nick950222 <504111818@11.com>
2019-12-10 12:49:18 +0800

    first commit

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,39 +1,172 @@
 import React from 'react';
+import {useState,useEffect,useCallback} from 'react'
 import RTCMultiConnection from '../../dist/RTCMultiConnection.min'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap';
 import 'socket.io'
 
 const style = require('./index.css')
-
+const publicRoomIdentifier = 'dashboard'
 const DashBoard : React.FC = ()=>{
-        return <div>
-    <div id="widget-container" style="position: fixed;bottom: 0;right: 0;left: 20%;height: 100%;border: 1px solid black; border-top:0; border-bottom: 0;"></div>
-<video id="screen-viewer" controls playsinline autoplay></video>
+  
+    const connection = useState(new RTCMultiConnection())
 
-<div style="width: 20%; height: 100%; position: absolute;left:0;">
-    <video id="main-video" controls playsinline autoplay></video>
-    <div id="other-videos"></div>
-    <hr/>
-    <div style="padding: 5px 10px;">
-        <div id="onUserStatusChanged"></div>
+
+    return <div> 
+    <header style={{marginBottom: '20px'}}>
+    <span className="logo-text">Dashboard Example</span>
+
+    <div style={{float: 'right', marginTop: '15px'}}>
+      <button className="btn btn-primary" data-toggle="modal" data-target="#startRoomModel">Create A New Room</button>
+      <button id="btn-show-join-hidden-room" className="top-span btn btn-secondary" data-toggle="modal" data-target="#joinRoomModel">Join A Room</button>
+      <span className="top-span">Active rooms: <span id="active-rooms">0</span></span>
     </div>
-
-    <div style="margin-top: 20px;position: absolute;bottom: 0;background: white; padding-bottom: 20px; width: 94%">
-        <div id="conversation-panel"></div>
-        <div id="key-press" style="text-align: right; display: none; font-size: 11px;">
-            <span style="vertical-align: middle;"></span>
-            <img src="https://www.webrtc-experiment.com/images/key-press.gif" style="height: 12px; vertical-align: middle;"/>
+  </header>
+   <div className="modal fade" id="startRoomModel" tabIndex={-1} role="dialog" aria-labelledby="startRoomModelLabel" aria-hidden="true"> 
+    <div className="modal-dialog" role="document">
+      <div className="modal-content">
+        <div className="modal-header">
+          <h5 className="modal-title" id="startRoomModelLabel">Create A New Room</h5>
+          <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
         </div>
-        <textarea id="txt-chat-message"></textarea>
-        <button class="btn btn-primary" id="btn-chat-message" disabled>Send</button>
-        <img id="btn-attach-file" src="https://www.webrtc-experiment.com/images/attach-file.png" title="Attach a File"/>
-        <img id="btn-share-screen" src="https://www.webrtc-experiment.com/images/share-screen.png" title="Share Your Screen"/>
-    </div>
+        <div className="modal-body" style={{ paddingBottom: 0}}>
+          <form>
+            <div className="form-group">
+              <p>
+                <label className="col-form-label">Enter Room ID:</label>
+                <input type="text" className="form-control" id="txt-roomid"></input>
+              </p>
 
-    <canvas id="temp-stream-canvas" style="display: none;"></canvas>
+              <p>
+                <label  className="col-form-label">Enter Your Name:</label>
+                <input type="text" className="form-control" id="txt-user-name"></input>
+              </p>
+
+              <p style={{display: 'none'}}>
+                <label className="col-form-label">Enter Room Password:</label>
+                <input type="text" className="form-control" id="txt-room-password"></input>
+              </p>
+
+              <a href="" style={{float: 'right', marginBottom: '10px' ,fontSize: '14px' }} onClick={()=>{}} >Show More Options</a>
+
+              <p className="more-options" style={{ marginBottom: 0,display: 'none'}}>
+                <label className="col-form-label"><input type="checkbox" id="chk-room-password"/> Set Room Password?</label>
+              </p>
+
+              <p className="more-options" style={{marginTop: 0, marginBottom: 0, display: 'none'}}>
+                <label className="col-form-label"><input type="checkbox" id="chk-hidden-room"/> Hidden Room? (Hide from the list)</label>
+              </p>
+            </div>
+          </form>
+        </div>
+        <div className="modal-footer">
+          <button type="button" className="btn" data-dismiss="modal">Close</button>
+          <button type="button" className="btn btn-primary" id="btn-create-room">Create</button>
+        </div>
+      </div>
     </div>
+  </div>
+
+  <div className="modal fade" id="joinRoomModel" tabIndex={-1} role="dialog" aria-labelledby="joinRoomModelLabel" aria-hidden="true">
+    <div className="modal-dialog" role="document">
+      <div className="modal-content">
+        <div className="modal-header">
+          <h5 className="modal-title" id="joinRoomModelLabel">Join A Room</h5>
+          <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div className="modal-body">
+          <form>
+            <div className="form-group">
+              <p>
+                <label  className="col-form-label">Enter Room ID:</label>
+                <input type="text" className="form-control" id="txt-roomid-hidden"/>
+              </p>
+
+              <p>
+                <label  className="col-form-label">Enter Your Name:</label>
+                <input type="text" className="form-control" id="txt-user-name-hidden"/>
+              </p>
+
+              <p style={{display: 'none'}}>
+                <label  className="col-form-label">Enter Room Password:</label>
+                <input type="text" className="form-control" id="txt-room-password-hidden"/>
+              </p>
+            </div>
+          </form>
+        </div>
+        <div className="modal-footer">
+          <button type="button" className="btn" data-dismiss="modal">Close</button>
+          <button type="button" className="btn btn-primary" id="btn-join-hidden-room">Join</button>
+        </div>
+      </div>
     </div>
-    }
+  </div>
+
+  <div id="confirm-box-topper" style={{display:'none',zIndex:99999999,top:0,left:0,bottom:0,right:0,width:'100%',height:'100%',position:'fixed',background:'#000000'}}></div>
+  <div id="alert-box" className="modal fade" style={{display:'none',zIndex:999999999999999}}>
+            <div className="modal-dialog">
+                <div className="modal-content">
+                    <div className="modal-header">
+                        <h5 className="modal-title" id="alert-title">Alert</h5>
+                        <button type="button" className="close btn-alert-close" data-dismiss="modal" aria-label="Close">
+                          <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div className="modal-body">
+                        <div id="alert-message" className="model-list"></div>
+                    </div>
+
+                    <div className="modal-footer">
+                        <p id="alert-special"></p>
+                        <button className="btn btn-primary btn-alert-close">Close</button>
+                    </div>
+                </div>
+            </div>
+  </div>
+
+  <div id="confirm-box" className="modal fade" style={{display:'none',zIndex:999999999999999}}>
+            <div className="modal-dialog">
+                <div className="modal-content">
+                    <div className="modal-header">
+                        <h5 className="modal-title" id="confirm-title">Please Confirm</h5>
+                        <button type="button" className="close btn-confirm-close" data-dismiss="modal" aria-label="Close">
+                          <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div id="confirm-message" className="modal-body"></div>
+
+                    <div className="modal-footer">
+                        <button className="btn btn-confirm-close" id="btn-confirm-close">Cancel</button>
+                        <button className="btn btn-primary" id="btn-confirm-action">Confirm</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+  <div style={{ minHeight: '400px'}}>
+    <table className="table">
+      <thead>
+        <tr>
+          <th>#</th>
+          <th>Room ID</th>
+          <th>Owner ID</th>
+          <th>Session</th>
+          <th>Extra</th>
+          <th>Participants</th>
+          <th>Join</th>
+        </tr>
+      </thead>
+      <tbody id="rooms-list"><tr><td colSpan={9}>No active room found for this demo.</td></tr></tbody>
+    </table>
+  </div>
+
+    <footer style={{textAlign: 'center'}}>Dashboard + Video Conferencing + Chat + File Sharing</footer>
+    </div>
+       
+}
 
 export default DashBoard;
--- src/pages/Room/index.tsx
+++ src/pages/Room/index.tsx
@@ -1,11 +1,41 @@
 import React from 'react';
 
+
+/*<div>
+    <div id="widget-container" style={{ position: 'fixed',bottom: 0,right: 0,left: '20%',height: '100%',border: '1px solid black',borderTop:0, borderBottom: 0}}></div>
+    <video id="screen-viewer" controls playsinline ></video>
+
+    <div style={{width: '20%', height: '100%', position: 'absolute',left:0}}>
+    <video id="main-video" controls playsinline  ></video>
+    <div id="other-videos"></div>
+    <hr/>
+    <div style="padding: 5px 10px;">
+        <div id="onUserStatusChanged"></div>
+    </div>
+
+    <div style="margin-top: 20px;position: absolute;bottom: 0;background: white; padding-bottom: 20px; width: 94%">
+        <div id="conversation-panel"></div>
+        <div id="key-press" style="text-align: right; display: none; font-size: 11px;">
+            <span style="vertical-align: middle;"></span>
+            <img src="https://www.webrtc-experiment.com/images/key-press.gif" style="height: 12px; vertical-align: middle;"/>
+        </div>
+        <textarea id="txt-chat-message"></textarea>
+        <button class="btn btn-primary" id="btn-chat-message" disabled>Send</button>
+        <img id="btn-attach-file" src="https://www.webrtc-experiment.com/images/attach-file.png" title="Attach a File"/>
+        <img id="btn-share-screen" src="https://www.webrtc-experiment.com/images/share-screen.png" title="Share Your Screen"/>
+    </div>
+
+    <canvas id="temp-stream-canvas" style="display: none;"></canvas>
+    </div>
+    </div>*/
+
+    
 const Room : React.FC = ()=>{
-        return (
-            <div>
-                <a href="#/">回dashboard</a>
-            </div>
-        )
+    return (
+        <div>
+            <a href="#/">回dashboard</a>
+        </div>
+    )
 }
 
 export default Room;
